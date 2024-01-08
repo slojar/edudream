befe2bd21b56eebf93e6dd96d150b6cc1dead61d
slojararshavin <sunday@tm30.net>
2024-01-08 02:43:37 +0100

    add site id

--- edudream/settings/base.py
+++ edudream/settings/base.py
@@ -133,3 +133,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
+SITE_ID = 1
+
