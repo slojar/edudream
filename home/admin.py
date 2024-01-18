79bc089fcd21a797625c89034da514405918c36c
slojararshavin <sunday@tm30.net>
2024-01-18 09:50:52 +0100

    update

--- home/admin.py
+++ home/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from home.models import SiteSetting, PaymentPlan, Profile, Language
+from home.models import SiteSetting, PaymentPlan, Profile, Language, Subject
 
 admin.site.register(SiteSetting)
 admin.site.register(PaymentPlan)
 admin.site.register(Language)
 admin.site.register(Profile)
+admin.site.register(Subject)
 
 
