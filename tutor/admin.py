ac25216e258575232990dc081ad0ad0d361aa6f4
slojararshavin <sunday@tm30.net>
2024-02-28 22:51:57 +0100

    update

--- tutor/admin.py
+++ tutor/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from tutor.models import TutorCalendar, Classroom
+from tutor.models import TutorCalendar, Classroom, TutorBankAccount
 
 admin.site.register(TutorCalendar)
 admin.site.register(Classroom)
+admin.site.register(TutorBankAccount)
 
