68b92add6f7872b50812ce8d9db847b265df3688
Sunday Olaofe <slojararshavin@gmail.com>
2024-10-10 15:02:52 +0100

    Add username to parentStudentSerializer

--- parent/serializers.py
+++ parent/serializers.py
@@ -16,6 +16,7 @@ class ParentStudentSerializerOut(serializers.ModelSerializer):
     first_name = serializers.CharField(source="user.first_name")
     last_name = serializers.CharField(source="user.last_name")
     email = serializers.CharField(source="user.email")
+    username = serializers.CharField(source="user.username")
     parent_name = serializers.CharField(source="parent.get_full_name")
     help_subject_names = serializers.SerializerMethodField()
 
